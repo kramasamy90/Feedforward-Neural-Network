9678b53576e8a8c6437991f1095cfab91d1f4806
Ramasamy Kandasamy <kramasamy90@gmail.com>
2023-03-17 13:22:39 +0530

    Implement batch gradient descent.

---
+++ src/gd.py
@@ -0,0 +1,28 @@
+import numpy as np
+import ann
+
+def get_batch(X_train, y_train, batch_size):
+    n_datapoints = y_train.shape[0]
+    random_indices = np.random.choice(n_datapoints, batch_size, replace=False)
+    X_train_batch = X_train[random_indices]
+    y_train_batch = y_train[random_indices]
+    return [X_train_batch, y_train_batch]
+
+def batch_gd(nn, eta, batch_size, X_train, y_train):
+    L = nn.n_hidden_layers + 1
+    X_train_batch, y_train_batch = get_batch(X_train, y_train, batch_size)
+    grad_Ws = []
+    grad_bs = []
+    for i in range(L):
+        grad_Ws.append(np.zeros([nn.Ws[i].shape[0], nn.Ws[i].shape[1]]))
+        grad_bs.append(np.zeros([nn.bs[i].shape[0], nn.bs[i].shape[1]]))
+    for i in range(batch_size):
+        nn.forward_prop(X_train_batch[i].flatten())
+        nn.back_prop(y_train_batch[i])
+        for i in range(L):
+            grad_Ws[i] += nn.grad_Ws[i]
+            grad_bs[i] += nn.grad_bs[i]
+    
+    for i in range(L):
+        nn.Ws[i] -= eta * grad_Ws[i] / batch_size
+        nn.bs[i] -= eta * grad_bs[i] / batch_size
