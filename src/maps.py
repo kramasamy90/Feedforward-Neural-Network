b727583e6a873acff653f4ea289e455cb212c8b0
Ramasamy Kandasamy <kramasamy90@gmail.com>
2023-03-18 11:25:23 +0530

    Initial commit.

---
+++ src/maps.py
@@ -0,0 +1,20 @@
+arg_short_to_long = {
+    'wp': 'wandb_project', # str
+    'we': 'wandb_entity', # str
+    'd': 'dataset', # 'mnist', 'fashion_mnist', 
+    'e': 'epochs', # int
+    'b': 'batch_size', # int
+    'l': 'loss', # ''mean_squared_error', 'cross_entropy'. 
+    'o': 'optimizer', # 'sgd, 'momentum', 'nag', 'rmsprop', 'adam', 'nadam'.
+    'lr': 'learning_rate', # float
+    'm': 'momentum', # float
+    'beta': 'beta', # float
+    'beta1': 'beta1', # float
+    'beta2': 'beta2', # float
+    'eps': 'epsilon', # float
+    'w_d': 'weight_decay', # float
+    'w_i': 'weight_init', # 'random', 'Xavier'
+    'nhl': 'num_layers', # int
+    'sz': 'hidden_size', # int
+    'a': 'activation' # identity,sigmoid, tanh, ReLU
+}
