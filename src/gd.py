a324153b255efd00c23c9069d3262aa3b963dd71
Ramasamy Kandasamy <kramasamy90@gmail.com>
2023-03-19 13:37:27 +0530

    Split batch gradient descent algorithm.

--- src/gd.py
+++ src/gd.py
@@ -8,7 +8,7 @@ def get_batch(X_train, y_train, batch_size):
     y_train_batch = y_train[random_indices]
     return [X_train_batch, y_train_batch]
 
-def batch_gd(nn, X_train, y_train):
+def compute_gradient(nn, X_train, y_train):
     X_train_batch, y_train_batch = get_batch(X_train, y_train, nn.batch_size)
     grad_Ws = []
     grad_bs = []
@@ -21,7 +21,10 @@ def batch_gd(nn, X_train, y_train):
         for i in range(ann.num_layers + 1):
             grad_Ws[i] += nn.grad_Ws[i]
             grad_bs[i] += nn.grad_bs[i]
-    
+    return grad_Ws, grad_bs
+
+def batch_gd(nn, X_train, y_train):
+    grad_Ws, grad_bs = compute_gradient(nn, X_train, y_train)    
     for i in range(ann.num_layers + 1):
         nn.Ws[i] -= ann.learning_rate * ann.weight_decay * nn.Ws[i]
         nn.Ws[i] -= ann.learning_rate * grad_Ws[i] / nn.batch_size
