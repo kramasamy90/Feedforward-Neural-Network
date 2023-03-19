89653847cff4299a0000f9f4592622cf57063ba5
Ramasamy Kandasamy <kramasamy90@gmail.com>
2023-03-19 12:19:09 +0530

    Correct a blunder. Original code was not updating weights in the last layer.

--- src/gd.py
+++ src/gd.py
@@ -12,17 +12,17 @@ def batch_gd(nn, X_train, y_train):
     X_train_batch, y_train_batch = get_batch(X_train, y_train, nn.batch_size)
     grad_Ws = []
     grad_bs = []
-    for i in range(ann.num_layers):
+    for i in range(ann.num_layers + 1):
         grad_Ws.append(np.zeros([nn.Ws[i].shape[0], nn.Ws[i].shape[1]]))
         grad_bs.append(np.zeros([nn.bs[i].shape[0], nn.bs[i].shape[1]]))
     for i in range(nn.batch_size):
         nn.forward_prop(X_train_batch[i].flatten())
         nn.back_prop(y_train_batch[i])
-        for i in range(ann.num_layers):
+        for i in range(ann.num_layers + 1):
             grad_Ws[i] += nn.grad_Ws[i]
             grad_bs[i] += nn.grad_bs[i]
     
-    for i in range(ann.num_layers):
+    for i in range(ann.num_layers + 1):
         nn.Ws[i] -= ann.learning_rate * ann.weight_decay * nn.Ws[i]
         nn.Ws[i] -= ann.learning_rate * grad_Ws[i] / nn.batch_size
         nn.bs[i] -= ann.learning_rate * ann.weight_decay * nn.bs[i]
