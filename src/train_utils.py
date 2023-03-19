c97e13352989771de3db8dfe4fb62d6b8dc1792b
Ramasamy Kandasamy <kramasamy90@gmail.com>
2023-03-19 19:04:50 +0530

    Add loss function.

--- src/train_utils.py
+++ src/train_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from ann import ann
 
 def get_classification_accuracy(nn, X_valid, y_valid):
@@ -8,3 +9,13 @@ def get_classification_accuracy(nn, X_valid, y_valid):
         if nn.predict(X_valid[i]) == y_valid[i]:
             correct += 1
     return correct / n
+
+def get_loss(nn, X, y):
+    n = len(y.flatten())
+    loss = 0.0
+    for i in range(n):
+        l = y[i]
+        nn.forward_prop(X[i])
+        loss += -math.log(nn.y[l])
+    
+    return loss / n
